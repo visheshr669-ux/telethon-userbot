043f76b8d6de60257507a232d6211e1716e1e712
visheshr669-ux <visheshr669@gmail.com>
2026-08-05 16:54:07 +0530

    Update stylish custom offline message

--- userbot.py
+++ userbot.py
@@ -1,5 +1,4 @@
 import os
-import asyncio
 from fastapi import FastAPI
 import uvicorn
 from telethon import TelegramClient, events
@@ -7,9 +6,12 @@ from telethon.sessions import StringSession
 
 app = FastAPI()
 
+AUTO_REPLY_TEXT = """𝙑𝙞𝙨𝙝𝙪 𝙞𝙨 𝙘𝙪𝙧𝙧𝙚𝙣𝙩𝙡𝙮 𝙤𝙛𝙛𝙡𝙞𝙣𝙚.🚫
+𝙎𝙞𝙡𝙚𝙣𝙘𝙚 𝙞𝙨𝙣'𝙩 𝙖𝙗𝙨𝙚𝙣𝙘𝙚—𝙞𝙩'𝙨 𝙛𝙤𝙘𝙪𝙨. 𝙇𝙚𝙖𝙫𝙚 𝙮𝙤𝙪𝙧 𝙢𝙚𝙨𝙨𝙖𝙜𝙚. 𝙄'𝙡𝙡 𝙧𝙚𝙥𝙡𝙮 𝙉𝙤𝙩 𝙚𝙫𝙚𝙧𝙮 𝙢𝙚𝙨𝙨𝙖𝙜𝙚 𝙙𝙚𝙨𝙚𝙧𝙫𝙚𝙨 𝙖𝙣 𝙞𝙣𝙨𝙩𝙖𝙣𝙩 𝙧𝙚𝙥𝙡𝙮. 𝙔𝙤𝙪𝙧𝙨 𝙝𝙖𝙨 𝙗𝙚𝙚𝙣 𝙧𝙚𝙘𝙚𝙞𝙫𝙚𝙙. 𝙬𝙝𝙚𝙣 𝙩𝙝𝙚 𝙩𝙞𝙢𝙚 𝙞𝙨 𝙧𝙞𝙜𝙝𝙩.𝙞𝙩 𝙬𝙞𝙡𝙡 𝙗𝙚 𝙨𝙚𝙚𝙣💭"""
+
 @app.get("/")
 def home():
-    return {"status": "UserBot is online and running!"}
+    return {"status": "UserBot is online!"}
 
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
@@ -22,12 +24,11 @@ async def pm_handler(event):
     if event.is_private:
         sender = await event.get_sender()
         if sender and not sender.bot and not sender.is_self:
-            await event.reply("Yooooo Vishu here!! wasupp !! See you within a minute!")
+            await event.reply(AUTO_REPLY_TEXT)
 
 @app.on_event("startup")
 async def startup_event():
     await client.start()
-    print("Telethon Client Started Successfully!")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
